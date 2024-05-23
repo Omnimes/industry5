0cf39f9e0e4285ebb6520543422f094e16e3e0af
Piotr Sierant <psierant@multiprojekt.pl>
2024-05-23 09:41:54 +0200

    poprawka kontaktu

--- src/app/[locale]/(marketing)/contact/page.tsx
+++ src/app/[locale]/(marketing)/contact/page.tsx
@@ -1,14 +1,25 @@
 import { ContactForm } from "@/components/Form";
 import { unstable_setRequestLocale } from "next-intl/server";
+import Image from "next/image";
 
 export default async function ContactPage({ params: { locale } }: { params: { locale: string } }) {
   // Enable static rendering
   unstable_setRequestLocale(locale);
 
   return (
-    <main>
-      <section className="mx-auto max-w-[500px] px-3 py-10 md:px-0">
+    <main className="mx-auto flex w-full max-w-5xl flex-wrap-reverse px-4">
+      <section className="mx-auto w-full max-w-[500px] py-10 md:px-0">
         <ContactForm />
+      </section>
+      <section className="mx-auto max-w-[500px] px-4 py-10 md:px-0">
+        <h4></h4>
+        <div className="text-center text-gray-500 dark:text-gray-400">
+              <Image className="mx-auto mb-4 size-36 rounded-full" src="/images/ceo.jfif" alt="Helene Avatar" width={144} height={144} />
+              <h3 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="#">dr inż. Grzegorz Góral</a>
+              </h3>
+              <p>ggoral@multiprojekt.pl</p>
+          </div>
       </section>
     </main>
   );
