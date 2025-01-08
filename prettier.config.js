bf4d51fca27b9bd340d99b62785dfcac042d2a7f
Piotr Sierant <piotr.sierant96@gmail.com>
2025-01-08 15:18:24 +0100

    modify config

--- prettier.config.js
+++ prettier.config.js
@@ -26,5 +26,5 @@ module.exports = {
     "^[./]",
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
 }
